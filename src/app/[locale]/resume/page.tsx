7d46f39d68bc8af422444a5d3aa7b8ed2074b5e8
assil28 <assil.dkhil28@gmail.com>
2025-09-10 16:50:14 +0200

    delete competence description

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -384,7 +384,6 @@ const Resume = () => {
                 <div className="flex flex-col gap-[30px] text-center xl:text-left">
                   <h3 className="text-4xl font-bold">{t("skills.title")}</h3>
                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                    {t("skills.description")}
                   </p>
                 </div>
                 <h4 className="text-2xl font-bold text-center xl:text-left">
